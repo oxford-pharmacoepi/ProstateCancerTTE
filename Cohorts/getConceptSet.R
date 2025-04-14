5e27b8991592a7120c017a12126cf570f03cc485
cecicampanile <campanile.cecilia@gmail.com>
2025-04-14 10:29:44 +0100

    diagnostics

--- Cohorts/getConceptSet.R
+++
@@ -1,4 +0,0 @@
-data_path <- file.path(getwd(), "Cohorts/prostate-cohorts")
-json_files <- list.files(data_path, pattern = "\\.json$", full.names = TRUE)
-
-conceptSet <- CodelistGenerator::codesFromConceptSet(data_path, cdm = cdm)
