cdce01e656163f7f7b2a89167cd2d3da7a6ddcf5
catalamarti <marticatalasabate@gmail.com>
2025-06-20 18:16:08 +0100

    comments

--- Cohorts/createCohorts.R
+++ Cohorts/createCohorts.R
@@ -1,25 +1,46 @@
 
 # import concepts ----
 
-folder_path <- here::here("Cohorts/ConceptSets")
+folder_path <- here::here("Cohorts", "ConceptSets")
 
-codelist <- omopgenerics::importCodelist(paste0(folder_path), "csv")
+codelist <- omopgenerics::importCodelist(path = folder_path, type = "csv")
 
-codelist_treatment <- list("EBRT" = codelist$`[OPTIMA RQ4] EBRT`, "Radical prostatectomy" = codelist$`[OPTIMA RQ4] Radical prostatectomy`)
+# TO CONSIDER TO CHANGE ALL THE CODELIST NAMES HERE
+# OR AT THE FILE LEVEL
 
-codelist_stage <- list("cT1-T2" = codelist$`[OPTIMA PCa RQ4] cT1-T2` , "Stage I-II" = codelist$`[OPTIMA PCa RQ4] Stage I-II`)
+codelist_treatment <- list(
+  "EBRT" = codelist$`[OPTIMA RQ4] EBRT`,
+  "Radical prostatectomy" = codelist$`[OPTIMA RQ4] Radical prostatectomy`
+)
 
-codelist$`[OPTIMA PCa RQ4] PSA` <- cdm$concept |> dplyr::filter(concept_id %in% codelist$`[OPTIMA PCa RQ4] PSA`) |> dplyr::filter(!grepl("free", tolower(.data$concept_name))) |> dplyr::pull(concept_id)
+codelist_stage <- list(
+  "cT1-T2" = codelist$`[OPTIMA PCa RQ4] cT1-T2` ,
+  "Stage I-II" = codelist$`[OPTIMA PCa RQ4] Stage I-II`
+)
+
+codelist$`[OPTIMA PCa RQ4] PSA` <- cdm$concept |>
+  dplyr::filter(concept_id %in% codelist$`[OPTIMA PCa RQ4] PSA`) |>
+  dplyr::filter(!grepl("free", tolower(.data$concept_name))) |>
+  dplyr::pull("concept_id")
 
 # concept cohorts ----
 
-cdm$prostate_cancer_age_50_69 <- CohortConstructor::conceptCohort(cdm,
-                                                                  conceptSet = list("Prostate cancer conditions" = codelist$`[OPTIMA RQ1] Prostate cancer conditions`),
-                                                                  name = "prostate_cancer_age_50_69" ) |>
-  CohortConstructor::requireDemographics(sex = "Male",
-                                         ageRange = list(c(50, 69)),
-                                         minPriorObservation = 365,
-                                         indexDate = "cohort_start_date")
+cdm$prostate_cancer_age_50_69 <- CohortConstructor::conceptCohort(
+  cdm = cdm,
+  conceptSet = list("prostate_cancer_conditions" = codelist$`[OPTIMA RQ1] Prostate cancer conditions`),
+  name = "prostate_cancer_age_50_69",
+  exit = "event_start_date"
+) |>
+  # SHOULD WE RESTRICT TO ONLY THE FIRST RECORD?
+  # require is first record
+  # CohortConstructor::requireIsFirstEntry() |>
+  # require demographics
+  CohortConstructor::requireDemographics(
+    sex = "Male",
+    ageRange = list(c(50, 69)),
+    minPriorObservation = 365, # WHY?
+    indexDate = "cohort_start_date"
+  )
 
 # cdm$prostate_cancer_first_diagnosis <- CohortConstructor::conceptCohort(cdm,
 #                                                                                conceptSet = list("Prostate cancer conditions" = codelist$`[OPTIMA RQ1] Prostate cancer conditions`),
@@ -32,52 +53,78 @@ cdm$prostate_cancer_age_50_69 <- CohortConstructor::conceptCohort(cdm,
 #                                                        name = "psa_cohort",
 #                                                        valueAsNumber = list(c(3, 19.99)) )
 
-cdm$psa_values <- CohortConstructor::measurementCohort(cdm,
-                                                conceptSet = list("PSA" = codelist$`[OPTIMA PCa RQ4] PSA`),
-                                                name = "psa_values",
-                                                valueAsNumber = list(c(-Inf, Inf))) |>
-  dplyr::left_join(cdm$measurement |>
-                     dplyr::filter(measurement_concept_id %in% codelist$`[OPTIMA PCa RQ4] PSA`) |>
-                     dplyr::select(person_id, measurement_concept_id, measurement_date, value_as_number, unit_source_value  ),
-                   by = c("subject_id" = "person_id", "cohort_start_date" = "measurement_date")
-
+cdm$psa_values <- CohortConstructor::measurementCohort(
+  cdm = cdm,
+  conceptSet = list("psa" = codelist$`[OPTIMA PCa RQ4] PSA`),
+  name = "psa_values",
+  valueAsNumber = list(c(-Inf, Inf))
+) |>
+  PatientProfiles::addConceptIntersectField(
+    conceptSet = codelist["[OPTIMA PCa RQ4] PSA"],
+    window = c(0, 0),
+    field = c("value_as_number", "unit_source_value"),
+    nameStyle = "{field}"
   ) |>
-  CohortConstructor::requireConceptIntersect(conceptSet = list("tr" = c(codelist$`[OPTIMA RQ4] EBRT`, codelist$`[OPTIMA RQ4] Radical prostatectomy`) ),
-                                             window = c(0, 180),
-                                             intersections = c(1, Inf)) |>
+  # dplyr::left_join(
+  #   cdm$measurement |>
+  #     dplyr::filter(measurement_concept_id %in% codelist$`[OPTIMA PCa RQ4] PSA`) |>
+  #     dplyr::select(person_id, measurement_concept_id, measurement_date, value_as_number, unit_source_value),
+  #   by = c("subject_id" = "person_id", "cohort_start_date" = "measurement_date")
+  # ) |>
+  CohortConstructor::requireConceptIntersect(
+    conceptSet = list("tr" = c(codelist$`[OPTIMA RQ4] EBRT`, codelist$`[OPTIMA RQ4] Radical prostatectomy`)),
+    window = c(0, 180),
+    intersections = c(1, Inf)
+  ) |>
   CohortConstructor::requireIsLastEntry()
 
 cdm$psa_trial <- cdm$psa_values |>
   dplyr::filter(.data$value_as_number <= 19.99 & .data$value_as_number >=3) |>
   dplyr::compute(name = "psa_trial")
 
-
-
-
-cdm$stage <- CohortConstructor::conceptCohort(cdm, conceptSet = list("c_t1_t2" = codelist$`[OPTIMA PCa RQ4] cT1-T2`), name = "stage") |>
-  CohortConstructor::requireConceptIntersect(conceptSet = list("MO" = codelist$`[OPTIMA PCa RQ4] M0`),
-                                             window = c(-Inf, Inf),
-                                             cohortId = "c_t1_t2",
-                                             intersections = c(1, Inf))
-
-PatientProfiles::addConceptIntersectField() |>
-  dplyr::filter() |>
-  dplyr::compute() |>
-  omopgenerics::recordCohortAttrition()
+cdm$stage <- CohortConstructor::conceptCohort(
+  cdm = cdm,
+  conceptSet = list("c_t1_t2" = codelist$`[OPTIMA PCa RQ4] cT1-T2`),
+  name = "stage",
+  exit = "event_start_date"
+) |>
+  CohortConstructor::requireConceptIntersect(
+    conceptSet = list("mo" = codelist$`[OPTIMA PCa RQ4] M0`),
+    window = c(-Inf, Inf),
+    cohortId = "c_t1_t2",
+    intersections = c(1, Inf)
+  )
 
 # tte ----
 
-cdm$optima_pc_trial <- CohortConstructor::conceptCohort(cdm, conceptSet = codelist_treatment, name = "optima_pc_trial") |>
-
-  CohortConstructor::requireIsFirstEntry() |>
-
+# create base cohort
+cdm$optima_pc_trial <- CohortConstructor::conceptCohort(
+  cdm = cdm,
+  conceptSet = codelist_treatment,
+  name = "optima_pc_trial",
+  exit = "event_start_date"
+) |>
+  # first entry
+  CohortConstructor::requireIsFirstEntry(name = "optima_pc_trial") |>
+  # only first entry per cohort
   dplyr::group_by(.data$subject_id) |>
-
   dplyr::slice_min(.data$cohort_start_date) |>
-
-  CohortConstructor::requireCohortIntersect(targetCohortTable = "prostate_cancer_age_50_69",
-                                            window = c(-180,0),
-                                            intersections = c(1, Inf)) |>
+  dplyr::compute(name = "optima_pc_trial") |>
+  omopgenerics::recordCohortAttrition(
+    reason = "Remove individuals with a prostatectomy",
+    cohortId = "EBRT"
+  ) |>
+  omopgenerics::recordCohortAttrition(
+    reason = "Remove individuals with radiotherapy before",
+    cohortId = "Radical prostatectomy"
+  ) |>
+  # prior record of prostate cancer
+  CohortConstructor::requireCohortIntersect(
+    targetCohortTable = "prostate_cancer_age_50_69",
+    window = c(-180, 0),
+    intersections = c(1, Inf)
+  ) |>
+  #  require psa value in the last 180 days
   CohortConstructor::requireCohortIntersect(
     targetCohortTable = "psa_trial",
     window = c(-180, 0),
@@ -88,6 +135,7 @@ cdm$optima_pc_trial <- CohortConstructor::conceptCohort(cdm, conceptSet = codeli
     targetStartDate = "cohort_start_date",
     targetEndDate = "cohort_end_date",
   ) |>
+  # require stage any time prior (m0 can be after)
   CohortConstructor::requireCohortIntersect(
     targetCohortTable = "stage",
     window = c(-Inf, 0),
@@ -97,6 +145,7 @@ cdm$optima_pc_trial <- CohortConstructor::conceptCohort(cdm, conceptSet = codeli
     targetStartDate = "cohort_start_date",
     targetEndDate = "cohort_end_date",
   ) |>
+  # to consider removing this line
   omopgenerics::recordCohortAttrition(reason = "at least one measurement of both cT1-T2 and M0 or of Stage I-II any day before cohort start date") |>
   CohortConstructor::requireConceptIntersect(
     conceptSet = list("Malignancy except non-melanoma skin cancer" = codelist$`Malignancy except non-melanoma skin cancer`),
