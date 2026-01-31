77030177a70f64ea448fe4735b916d1995187071
catalamarti <marticatalasabate@gmail.com>
2026-01-31 20:49:50 +0000

    categories

--- CloneCensorWeight/Analyses/CalculateWeights.R
+++ CloneCensorWeight/Analyses/CalculateWeights.R
@@ -2,6 +2,9 @@
 minFrequency <- 0.005
 excludeCodes <- c(0, unlist(exclude, use.names = FALSE), codelist$radiotheraphy, codelist$prostatectomy)
 
+cdm$my_cohort  <- cdm$my_cohort  |>
+  mutate(index_year = get_year(cohort_start_date)) |>
+  compute(name = "my_cohort")
 individuals <- cdm$my_cohort |>
   group_by(subject_id, cohort_start_date) |>
   summarise(max_censor = max(follow_up), .groups = "drop") |>
@@ -25,7 +28,16 @@ psa <- cdm$psa |>
   ) |>
   mutate(time = date_count_between(index_date, cohort_start_date)) |>
   select(subject_id, time, psa_category) |>
-  collect()
+  collect() |>
+  mutate(psa_category = case_when(
+    psa_category == "[0, 3)" ~ 0,
+    psa_category == "[3, 6)" ~ 1,
+    psa_category == "[6, 10)" ~ 2,
+    psa_category == "[10, 20)" ~ 3,
+    psa_category == "[20, 40)" ~ 4,
+    psa_category == "[40, Inf)" ~ 5,
+    .default = NA
+  ))
 gleason <- cdm$gleason |>
   inner_join(
     cdm$my_cohort |>
@@ -35,4 +47,12 @@ gleason <- cdm$gleason |>
   ) |>
   mutate(time = date_count_between(index_date, cohort_start_date)) |>
   select(subject_id, time, gleason_category) |>
-  collect()
+  collect() |>
+  mutate(gleason_category = case_when(
+    gleason_category == "<2" ~ 0,
+    gleason_category == "2 to 6" ~ 1,
+    gleason_category == "7" ~ 2,
+    gleason_category == "8 to 10" ~ 3,
+    gleason_category == ">10" ~ 4,
+    .default = NA
+  ))
